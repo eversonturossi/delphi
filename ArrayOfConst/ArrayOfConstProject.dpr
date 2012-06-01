a76378027ac967582eb967f2bd667f514e6ea8c9
cust <eversont@gmail.com>
2012-06-01 18:47:23 -0300

    ...

---
+++ ArrayOfConst/ArrayOfConstProject.dpr
@@ -0,0 +1,15 @@
+program ArrayOfConstProject;
+
+uses
+  Forms,
+  Unit1 in 'Unit1.pas' { Form1 } ,
+  UTopMessageBox in 'UTopMessageBox.pas';
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+
+end.
