e9f3306fc031b2cb95e4fc4cbc943e46dba972eb
cust <eversont@gmail.com>
2012-05-31 23:04:45 -0300

    update

---
+++ UpdateApp/UpdateServer.dpr
@@ -0,0 +1,14 @@
+program UpdateServer;
+
+uses
+  Forms,
+  UUpdateServer in 'UUpdateServer.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
