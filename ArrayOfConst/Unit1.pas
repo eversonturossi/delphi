7145b72860943ba98a09b1474d80c35beb007e07
cust <eversont@gmail.com>
2012-06-01 18:47:14 -0300

    ..

--- ArrayOfConst/Unit1.pas
+++ ArrayOfConst/Unit1.pas
@@ -1,10 +1,9 @@
 unit Unit1;
-
-interface
+interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls;
+  Dialogs, StdCtrls, UTopMessageBox;
 
 type
   TForm1 = class(TForm)
@@ -22,98 +21,6 @@ var
 implementation
 
 {$R *.dfm}
-
-function Converte(Arg: TVarRec): String;
-var
-  PonteiroWideString: ^widestring;
-  PonteiroWideChar: ^WideChar;
-  PonteiroExtended: ^Extended;
-  PonteiroVariant: ^Variant;
-begin
-  Result := 'nulo';
-  case Arg.VType of
-    vtInteger:
-      Result := IntToStr(Arg.VInteger); // ok
-    vtBoolean:
-      Result := BoolToStr(Arg.VBoolean); // ok
-    vtChar:
-      Result := Arg.VChar; // nao usado
-    vtExtended:
-      begin
-        PonteiroExtended := @Arg.vExtended;
-        // vExtended := PonteiroExtended^;
-        // Result := FloatToStr(PonteiroExtended^);
-      end;
-    vtString:
-      Result := String(Arg.VString);
-    vtPointer:
-      Result := 'ponteiro';
-    vtPChar:
-      Result := Arg.VPChar;
-    vtObject:
-      Result := String(Arg.VObject);
-    vtClass:
-      Result := Arg.VClass.ClassName;
-    vtWideChar:
-      Result := Arg.VWideChar; // ok e tambem usado pelo ansichar
-    vtPWideChar:
-      begin
-        PonteiroWideChar := @Arg.VPWideChar;
-        // Result := Arg.VPWideChar^;
-        // Result := WideCharToString(Arg.VPWideChar);
-      end;
-    vtAnsiString:
-      Result := String(Arg.VAnsiString);
-    vtCurrency:
-      Result := String(Arg.VCurrency);
-    vtVariant:
-      begin
-        PonteiroVariant := @Arg.VVariant;
-        Result := VarToStr(PonteiroVariant^);
-        // Result := String(Arg.VVariant);
-      end;
-    vtInterface:
-      Result := 'interface';
-    vtWideString:
-      begin
-        PonteiroWideString := @Arg.VWideString;
-        Result := PonteiroWideString^;
-      end;
-    vtInt64:
-      Result := String(Arg.VInt64);
-    vtUnicodeString:
-      Result := string(Arg.VUnicodeString);
-  else
-    ShowMessage(IntToStr(Arg.VType));
-  end;
-end;
-
-function MensagemEscolha(AOwner: TComponent; Titulo, Mensagem: String; Args: array of const ): String;
-var
-  Form: TForm;
-  I: Integer;
-  TypeStr: String;
-begin
-  Result := 'null';
-  try
-    Form := TForm.Create(AOwner);
-    Form.BorderStyle := bsDialog;
-    Form.Position := poDesktopCenter;
-    Form.Caption := Titulo;
-    Form.ShowModal;
-
-    I := Low(Args);
-    while (I < High(Args)) do
-    begin
-      ShowMessage(Converte(Args[I]));
-      inc(I, 1);
-    end;
-
-  finally
-    FreeAndNil(Form);
-  end;
-end;
-
 { TForm1 }
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -135,6 +42,8 @@ var
   aWideString: widestring;
   aInt64: Int64;
   aUnicodeString: UnicodeString;
+
+  Mensagem: TTopMessageBox;
 begin
 
   aInteger := 10;
@@ -155,8 +64,13 @@ begin
   aInt64 := StrToInt64(' 1234');
   aUnicodeString := UnicodeString('unicodestring');
 
-  ShowMessage(MensagemEscolha(Self, '', '', [aInteger, aBoolean, aChar, aExtended, aString, aPointer, aPChar, aObject, aClass, aWideChar, aPWideChar,
-      aAnsiString, aCurrency, aVariant, aWideString, aInt64, aUnicodeString]));
+  // ShowMessage(MensagemEscolha(Self, '', '', [aInteger, aBoolean, aChar, aExtended, aString, aPointer, aPChar, aObject, aClass, aWideChar, aPWideChar,
+  // aAnsiString, aCurrency, aVariant, aWideString, aInt64, aUnicodeString]));
+  Mensagem := TTopMessageBox.Create;
+  Mensagem.Opcao := 100000;
+  ShowMessage(Mensagem.MensagemEscolha(Self, '', '', ['1', '2', '3', '4']));
+  FreeAndNil(Mensagem);
 end;
 
 end.
+
