03b3b5f218d3b8dce939c3899224eb8d55636479
Everson Turossi <eversont@gmail.com>
2014-07-08 09:45:46 -0300

    xxxx

--- RelatorioConfiguravel/RelatorioConfiguravel.dpr
+++ RelatorioConfiguravel/RelatorioConfiguravel.dpr
@@ -2,14 +2,15 @@ program RelatorioConfiguravel;
 
 uses
   Forms,
-  Unit1 in 'Unit1.pas' {Form1},
-  Relatorio_NovaImpressao in 'Relatorio_NovaImpressao.pas' {RelatorioNovaImpressao: TQuickRep};
-
+  Unit1 in 'Unit1.pas' { Form1 } ,
+  Relatorio_NovaImpressao in 'Relatorio_NovaImpressao.pas' { RelatorioNovaImpressao: TQuickRep } ;
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  ReportMemoryLeaksOnShutdown := DebugHook <> 0;
   Application.CreateForm(TForm1, Form1);
   Application.Run;
+
 end.
