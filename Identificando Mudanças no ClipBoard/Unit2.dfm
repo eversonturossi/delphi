3e27b60afe21ffc6b431940aabcbe52d40b5c244
Everson Turossi <eversont@gmail.com>
2014-09-25 14:06:24 -0300

    mod

--- Identificando Mudanças no ClipBoard/Unit2.dfm
+++ Identificando Mudanças no ClipBoard/Unit2.dfm
@@ -12,19 +12,55 @@ object Form2: TForm2
   Font.Style = []
   OldCreateOrder = False
   OnCreate = FormCreate
-  DesignSize = (
-    770
-    463)
   PixelsPerInch = 96
   TextHeight = 13
-  object Memo1: TMemo
-    Left = 8
-    Top = 8
-    Width = 754
-    Height = 447
-    Anchors = [akLeft, akTop, akRight, akBottom]
-    Lines.Strings = (
-      'Memo1')
+  object PageControl1: TPageControl
+    Left = 0
+    Top = 0
+    Width = 770
+    Height = 463
+    ActivePage = TabSheet1
+    Align = alClient
     TabOrder = 0
+    ExplicitLeft = 248
+    ExplicitTop = 152
+    ExplicitWidth = 289
+    ExplicitHeight = 193
+    object TabSheet1: TTabSheet
+      Caption = 'Texto'
+      ExplicitWidth = 281
+      ExplicitHeight = 165
+      object Memo1: TMemo
+        Left = 0
+        Top = 0
+        Width = 762
+        Height = 435
+        Align = alClient
+        Lines.Strings = (
+          'Memo1')
+        TabOrder = 0
+        ExplicitLeft = -473
+        ExplicitTop = 30
+        ExplicitWidth = 754
+        ExplicitHeight = 135
+      end
+    end
+    object TabSheet2: TTabSheet
+      Caption = 'Imagem'
+      ImageIndex = 1
+      ExplicitWidth = 281
+      ExplicitHeight = 165
+      object Image1: TImage
+        Left = 0
+        Top = 0
+        Width = 762
+        Height = 435
+        Align = alClient
+        ExplicitLeft = 328
+        ExplicitTop = 168
+        ExplicitWidth = 105
+        ExplicitHeight = 105
+      end
+    end
   end
 end
--- Identificando Mudanças no ClipBoard/Unit2.pas
+++ Identificando Mudanças no ClipBoard/Unit2.pas
@@ -4,11 +4,15 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, Clipbrd;
+  Dialogs, StdCtrls, Clipbrd, ComCtrls, ExtCtrls;
 
 type
   TForm2 = class(TForm)
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
     Memo1: TMemo;
+    Image1: TImage;
     procedure FormCreate(Sender: TObject);
   private
     procedure ClipBoardChanged(var Message: TMessage); Message WM_DRAWCLIPBOARD;
@@ -23,12 +27,27 @@ implementation
 
 {$R *.dfm}
 
-{ Fonte: http://www.devmedia.com.br/identificando-mudancas-no-clipboard-delphi/24036 }
+{ Fontes
+  http://www.devmedia.com.br/identificando-mudancas-no-clipboard-delphi/24036
+  http://delphi.about.com/od/vclusing/a/tclipboard.htm
+  }
 procedure TForm2.ClipBoardChanged(var Message: TMessage);
 begin { uses Clipbrd }
-  Memo1.Lines.Clear;
-  Memo1.Lines.Add(Clipboard.AsText);
-  Self.Caption := FormatDateTime('hh:nn:ss', Now);
+  // CF_TEXT - Text with each line ending with a CR-LF combination.
+  // CF_BITMAP - A Windows bitmap graphic.
+  // CF_METAFILEPICT - A Windows metafile graphic.
+  // CF_PICTURE - An object of type TPicture.
+  // CF_OBJECT - Any persistent object.
+  if Clipboard.HasFormat(CF_TEXT) then
+  begin
+    Memo1.Lines.Clear;
+    Memo1.Lines.Add(Clipboard.AsText);
+    Self.Caption := FormatDateTime('hh:nn:ss', Now);
+  end;
+  if Clipboard.HasFormat(CF_BITMAP) then
+  begin
+    Image1.Picture.Bitmap.Assign(Clipboard);
+  end;
 end;
 
 procedure TForm2.FormCreate(Sender: TObject);
