97a981de25e3b128080423bee5cce6a8db157dc8
Everson <eversont@gmail.com>
2019-03-20 11:40:59 -0300

    certificado em string no acbr

---
+++ Certificado em String ACBr/Unit7.dfm
@@ -0,0 +1,123 @@
+object Form7: TForm7
+  Left = 0
+  Top = 0
+  Caption = 'Form7'
+  ClientHeight = 693
+  ClientWidth = 834
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 8
+    Top = 21
+    Width = 96
+    Height = 13
+    Caption = 'Caminho Certificado'
+  end
+  object Label2: TLabel
+    Left = 8
+    Top = 77
+    Width = 30
+    Height = 13
+    Caption = 'Senha'
+  end
+  object Label3: TLabel
+    Left = 352
+    Top = 189
+    Width = 117
+    Height = 13
+    Caption = 'Certificado em BASE64'
+  end
+  object Label4: TLabel
+    Left = 408
+    Top = 360
+    Width = 31
+    Height = 13
+    Caption = 'Label4'
+  end
+  object Label5: TLabel
+    Left = 8
+    Top = 189
+    Width = 93
+    Height = 13
+    Caption = 'Resultado Consulta'
+  end
+  object EditSenha: TEdit
+    Left = 8
+    Top = 96
+    Width = 441
+    Height = 21
+    TabOrder = 0
+    Text = 'EditSenha'
+  end
+  object EditCertificado: TEdit
+    Left = 8
+    Top = 40
+    Width = 441
+    Height = 21
+    TabOrder = 1
+    Text = 'EditCertificado'
+  end
+  object ButtonSelecionaCertificado: TButton
+    Left = 455
+    Top = 38
+    Width = 25
+    Height = 25
+    Caption = '...'
+    TabOrder = 2
+    OnClick = ButtonSelecionaCertificadoClick
+  end
+  object ButtonConsultaStatus: TButton
+    Left = 80
+    Top = 136
+    Width = 273
+    Height = 25
+    Caption = 'Consulta Status'
+    TabOrder = 3
+    OnClick = ButtonConsultaStatusClick
+  end
+  object MemoDados: TMemo
+    Left = 8
+    Top = 208
+    Width = 321
+    Height = 465
+    Lines.Strings = (
+      'MemoDados')
+    TabOrder = 4
+  end
+  object Memo1: TMemo
+    Left = 352
+    Top = 208
+    Width = 474
+    Height = 465
+    Lines.Strings = (
+      'Memo1')
+    TabOrder = 5
+  end
+  object ACBrNFe1: TACBrNFe
+    Configuracoes.Geral.SSLLib = libNone
+    Configuracoes.Geral.SSLCryptLib = cryNone
+    Configuracoes.Geral.SSLHttpLib = httpNone
+    Configuracoes.Geral.SSLXmlSignLib = xsNone
+    Configuracoes.Geral.FormatoAlerta = 'TAG:%TAGNIVEL% ID:%ID%/%TAG%(%DESCRICAO%) - %MSG%.'
+    Configuracoes.Geral.VersaoDF = ve400
+    Configuracoes.Geral.VersaoQRCode = veqr000
+    Configuracoes.Arquivos.OrdenacaoPath = <>
+    Configuracoes.WebServices.UF = 'SP'
+    Configuracoes.WebServices.AguardarConsultaRet = 0
+    Configuracoes.WebServices.QuebradeLinha = '|'
+    Configuracoes.WebServices.SSLType = LT_TLSv1_2
+    Left = 264
+    Top = 320
+  end
+  object OpenDialog1: TOpenDialog
+    Left = 408
+    Top = 352
+  end
+end
---
+++ Certificado em String ACBr/Unit7.pas
@@ -0,0 +1,89 @@
+unit Unit7;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, ACBrBase, ACBrDFe, ACBrNFe;
+
+type
+  TForm7 = class(TForm)
+    ACBrNFe1: TACBrNFe;
+    EditSenha: TEdit;
+    EditCertificado: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    ButtonSelecionaCertificado: TButton;
+    OpenDialog1: TOpenDialog;
+    ButtonConsultaStatus: TButton;
+    MemoDados: TMemo;
+    Memo1: TMemo;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    procedure ButtonSelecionaCertificadoClick(Sender: TObject);
+    procedure ButtonConsultaStatusClick(Sender: TObject);
+  private
+  public
+  end;
+
+var
+  Form7: TForm7;
+
+implementation
+
+uses
+  ACBrDFeConfiguracoes, ACBrDFeSSL, synautil, pcnConversao, synacode;
+
+{$R *.dfm}
+
+function GetCertificado(APFXFile: String): AnsiString;
+var
+  FS: TFileStream;
+begin
+  FS := TFileStream.Create(APFXFile, fmOpenRead);
+  try
+    Result := ReadStrFromStream(FS, FS.Size); // de USES no pacote synautil
+  finally
+    FS.Free;
+  end;
+end;
+
+procedure TForm7.ButtonConsultaStatusClick(Sender: TObject);
+var
+  LCertificadoBase64: AnsiString;
+begin
+  ACBrNFe1.Configuracoes.Geral.SSLLib := libWinCrypt;
+  ACBrNFe1.Configuracoes.Certificados.DadosPFX := GetCertificado(EditCertificado.Text);
+  ACBrNFe1.Configuracoes.Certificados.Senha := EditSenha.Text;
+  ACBrNFe1.Configuracoes.Arquivos.PathSchemas := 'C:\ACBr\trunk2\Exemplos\ACBrDFe\Schemas\NFe';
+
+  ACBrNFe1.Configuracoes.WebServices.UF := 'SC';
+  ACBrNFe1.Configuracoes.WebServices.Ambiente := taHomologacao;
+
+  LCertificadoBase64 := EncodeBase64(ACBrNFe1.Configuracoes.Certificados.DadosPFX); // uses  synacode
+  Memo1.Lines.Add(LCertificadoBase64);
+
+  ACBrNFe1.WebServices.StatusServico.Executar;
+
+  MemoDados.Lines.clear;
+  MemoDados.Lines.Add('');
+  MemoDados.Lines.Add('Status Serviço');
+  MemoDados.Lines.Add('tpAmb: ' + TpAmbToStr(ACBrNFe1.WebServices.StatusServico.tpAmb));
+  MemoDados.Lines.Add('verAplic: ' + ACBrNFe1.WebServices.StatusServico.verAplic);
+  MemoDados.Lines.Add('cStat: ' + IntToStr(ACBrNFe1.WebServices.StatusServico.cStat));
+  MemoDados.Lines.Add('xMotivo: ' + ACBrNFe1.WebServices.StatusServico.xMotivo);
+  MemoDados.Lines.Add('cUF: ' + IntToStr(ACBrNFe1.WebServices.StatusServico.cUF));
+  MemoDados.Lines.Add('dhRecbto: ' + DateTimeToStr(ACBrNFe1.WebServices.StatusServico.dhRecbto));
+  MemoDados.Lines.Add('tMed: ' + IntToStr(ACBrNFe1.WebServices.StatusServico.TMed));
+  MemoDados.Lines.Add('dhRetorno: ' + DateTimeToStr(ACBrNFe1.WebServices.StatusServico.dhRetorno));
+  MemoDados.Lines.Add('xObs: ' + ACBrNFe1.WebServices.StatusServico.xObs);
+end;
+
+procedure TForm7.ButtonSelecionaCertificadoClick(Sender: TObject);
+begin
+  if OpenDialog1.Execute then
+    EditCertificado.Text := OpenDialog1.FileName;
+end;
+
+end.
