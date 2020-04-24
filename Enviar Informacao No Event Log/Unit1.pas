80c23868cd5b9bb23a4bdc9d07f57046c366d5bb
Everson Turossi <everson.turossi@controlsoft.com.br>
2020-04-24 15:33:39 -0300

    informações no event log

---
+++ Enviar Informacao No Event Log/Unit1.pas
@@ -0,0 +1,42 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+  public
+    class procedure LogDebug(AMensagem: String);
+  end;
+
+function IsDebuggerPresent: LongBool; external kernel32 name 'IsDebuggerPresent';
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  LogDebug('teste teste teste');
+end;
+
+class procedure TForm1.LogDebug(AMensagem: String);
+var
+  LHora: String;
+begin
+  LHora := FormatDateTime('hh:mm:ss.zzz', now);
+  if IsDebuggerPresent then
+    OutputDebugString(PWideChar(LHora + '->' + AMensagem));
+end;
+
+end.
