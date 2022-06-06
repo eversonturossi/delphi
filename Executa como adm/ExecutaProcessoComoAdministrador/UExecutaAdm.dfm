882b387aef394907de6a573e24708b2f5bfadb4f
Everson Turossi <everson.turossi@controlsoft.com.br>
2022-06-06 11:06:44 -0300

    update

--- Executa como adm/ExecutaProcessoComoAdministrador/UExecutaAdm.dfm
+++ Executa como adm/ExecutaProcessoComoAdministrador/UExecutaAdm.dfm
@@ -13,6 +13,20 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 336
+    Top = 77
+    Width = 22
+    Height = 13
+    Caption = 'ADM'
+  end
+  object Label2: TLabel
+    Left = 512
+    Top = 77
+    Width = 45
+    Height = 13
+    Caption = 'Sem ADM'
+  end
   object Button1: TButton
     Left = 40
     Top = 96
@@ -30,5 +44,49 @@ object Form1: TForm1
     Caption = 'Sem ADM'
     TabOrder = 1
     OnClick = Button2Click
+  end
+  object ButtonStart: TButton
+    Left = 336
+    Top = 96
+    Width = 75
+    Height = 25
+    Caption = 'start'
+    TabOrder = 2
+    OnClick = ButtonStartClick
+  end
+  object ButtonStop: TButton
+    Left = 336
+    Top = 127
+    Width = 75
+    Height = 25
+    Caption = 'stop'
+    TabOrder = 3
+    OnClick = ButtonStopClick
+  end
+  object Button3: TButton
+    Left = 512
+    Top = 96
+    Width = 75
+    Height = 25
+    Caption = 'start'
+    TabOrder = 4
+    OnClick = Button3Click
+  end
+  object Button4: TButton
+    Left = 512
+    Top = 127
+    Width = 75
+    Height = 25
+    Caption = 'stop'
+    TabOrder = 5
+    OnClick = Button4Click
+  end
+  object edtServiceName: TEdit
+    Left = 336
+    Top = 32
+    Width = 121
+    Height = 21
+    TabOrder = 6
+    Text = 'ServiceName'
   end
 end
--- Executa como adm/ExecutaProcessoComoAdministrador/UExecutaAdm.pas
+++ Executa como adm/ExecutaProcessoComoAdministrador/UExecutaAdm.pas
@@ -10,8 +10,19 @@ type
   TForm1 = class(TForm)
     Button1: TButton;
     Button2: TButton;
+    ButtonStart: TButton;
+    ButtonStop: TButton;
+    Label1: TLabel;
+    Button3: TButton;
+    Button4: TButton;
+    Label2: TLabel;
+    edtServiceName: TEdit;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
+    procedure ButtonStartClick(Sender: TObject);
+    procedure ButtonStopClick(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -27,25 +38,25 @@ uses
   ShellApi;
 {$R *.dfm}
 
-procedure TForm1.Button1Click(Sender: TObject);
-  function RunAsAdmin(hWnd: hWnd; filename: string; Parameters: string): Boolean;
-  { See Step 3: Redesign for UAC Compatibility (UAC)
-    http://msdn.microsoft.com/en-us/library/bb756922.aspx }
-  var
-    sei: TShellExecuteInfo;
-  begin
-    ZeroMemory(@sei, SizeOf(sei));
-    sei.cbSize := SizeOf(TShellExecuteInfo);
-    sei.Wnd := hWnd;
-    sei.fMask := SEE_MASK_FLAG_DDEWAIT or SEE_MASK_FLAG_NO_UI;
-    sei.lpVerb := PChar('runas');
-    sei.lpFile := PChar(filename); // PAnsiChar;
-    if Parameters <> '' then
-      sei.lpParameters := PChar(Parameters); // PAnsiChar;
-    sei.nShow := SW_SHOWNORMAL; // Integer;
-    Result := ShellExecuteEx(@sei);
-  end;
+function RunAsAdmin(hWnd: hWnd; filename: string; Parameters: string): Boolean;
+{ See Step 3: Redesign for UAC Compatibility (UAC)
+  http://msdn.microsoft.com/en-us/library/bb756922.aspx }
+var
+  sei: TShellExecuteInfo;
+begin
+  ZeroMemory(@sei, SizeOf(sei));
+  sei.cbSize := SizeOf(TShellExecuteInfo);
+  sei.Wnd := hWnd;
+  sei.fMask := SEE_MASK_FLAG_DDEWAIT or SEE_MASK_FLAG_NO_UI;
+  sei.lpVerb := PChar('runas');
+  sei.lpFile := PChar(filename); // PAnsiChar;
+  if Parameters <> '' then
+    sei.lpParameters := PChar(Parameters); // PAnsiChar;
+  sei.nShow := SW_SHOWNORMAL; // Integer;
+  Result := ShellExecuteEx(@sei);
+end;
 
+procedure TForm1.Button1Click(Sender: TObject);
 begin
   { http://edn.embarcadero.com/article/33942 }
   { http://zewaren.net/site/node/10 }
@@ -55,6 +66,26 @@ end;
 procedure TForm1.Button2Click(Sender: TObject);
 begin
   ShellExecute(Application.Handle, 'open', 'cmd.exe', '', '', SW_SHOWNORMAL);
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  ShellExecute(Application.Handle, 'open', 'sc', 'start ' + edtServiceName.Name, '', SW_SHOWNORMAL);
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+  ShellExecute(Application.Handle, 'open', 'sc', 'stop ' + edtServiceName.Name, '', SW_SHOWNORMAL);
+end;
+
+procedure TForm1.ButtonStartClick(Sender: TObject);
+begin
+  RunAsAdmin(Application.Handle, 'sc', 'start ' + edtServiceName.Name);
+end;
+
+procedure TForm1.ButtonStopClick(Sender: TObject);
+begin
+  RunAsAdmin(Application.Handle, 'sc', 'stop ' + edtServiceName.Name);
 end;
 
 end.
